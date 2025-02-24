fc00dc30a005b499635b224dc3e1765b395a95ff
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-02-25 00:50:55 +0600

    add post provider to the rootprovider

--- src/contexts/RootProvider.tsx
+++ src/contexts/RootProvider.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import AuthProvider from "./AuthProvider";
+import PostProvider from "./PostProvider";
 
 export default function RootProvider({
 	children,
 }: Readonly<{ children: React.ReactNode }>) {
 	return (
 		<AuthProvider>
-			{children}
+			<PostProvider>{children}</PostProvider>
 		</AuthProvider>
 	);
 }
