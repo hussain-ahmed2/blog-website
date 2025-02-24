454fc26907d5276e333c02a43b62e5aabbb6f846
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-02-25 00:51:24 +0600

    create post context

---
+++ src/contexts/PostContext.ts
@@ -0,0 +1,18 @@
+import { Comment, Like, Post } from "@/types";
+import { createContext } from "react";
+
+export type PostContextType = {
+    handleCreatePost: (userId: number, title: string, body: string) => void;
+    handleUpdatePost: (postId: number, title: string, body: string) => void;
+    handleDeletePost: (postId: number) => void;
+    getPosts: (limit: number) => Post[];
+    handleLikePost: (postId: number, userId: number) => void;
+    handleCommentPost: (postId: number, userId: number, body: string) => void;
+    getLikes: (postId: number) => Like[];
+    getComments: (postId: number) => Comment[];
+    getPostById: (id: number) => Post | undefined;
+};
+
+const PostContext = createContext<PostContextType>({} as PostContextType);
+
+export default PostContext;
