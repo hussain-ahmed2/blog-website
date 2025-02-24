ce44698357047d62596be2e73978b3227278cb8f
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-02-24 21:30:32 +0600

    create authcontext

---
+++ src/contexts/AuthContext.ts
@@ -0,0 +1,15 @@
+import { RegisterUserData } from "@/form-schema/registerSchema";
+import { User } from "@/types";
+import { createContext } from "react";
+import { HandleLoginResponse } from "./AuthProvider";
+
+export interface AuthContextType {
+	user: User;
+    handleRegister: (userData: Omit<RegisterUserData, "confirmPassword">) => boolean;
+    handleLogin: (email: string, password: string) => HandleLoginResponse;
+    handleLogout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType>({} as AuthContextType);
+
+export default AuthContext;
