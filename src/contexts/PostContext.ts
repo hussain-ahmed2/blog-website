0805c39ba563eb6e67f2d18a80236d94e6a5548a
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-02-25 14:12:24 +0600

    add islikedbyuser function to the interface type

--- src/contexts/PostContext.ts
+++ src/contexts/PostContext.ts
@@ -5,12 +5,13 @@ export type PostContextType = {
     handleCreatePost: (userId: number, title: string, body: string) => void;
     handleUpdatePost: (postId: number, title: string, body: string) => void;
     handleDeletePost: (postId: number) => void;
-    getPosts: (limit: number) => Post[];
+    getPosts: (limit?: number) => Post[];
     handleLikePost: (postId: number, userId: number) => void;
     handleCommentPost: (postId: number, userId: number, body: string) => void;
     getLikes: (postId: number) => Like[];
     getComments: (postId: number) => Comment[];
     getPostById: (id: number) => Post | undefined;
+    isLikedByUser: (postId: number, userId: number) => boolean;
 };
 
 const PostContext = createContext<PostContextType>({} as PostContextType);
