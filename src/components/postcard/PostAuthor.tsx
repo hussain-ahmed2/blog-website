c4a82f3b2e49c870bdcbfdd9e8cd6d8a35dce11c
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-02-25 20:42:10 +0600

    if current author created the post then show the edit btn

--- src/components/postcard/PostAuthor.tsx
+++ src/components/postcard/PostAuthor.tsx
@@ -1,17 +1,28 @@
 import AuthContext from "@/contexts/AuthContext";
 import { User } from "@/types";
+import { Edit } from "lucide-react";
 import Link from "next/link";
 import { useContext } from "react";
 
-export default function PostAuthor({ author }: { author: User | null }) {
+export default function PostAuthor({ author, postId }: { author: User | null; postId: number; }) {
 	const { user } = useContext(AuthContext);
 
 	return (
-		<Link
-			href={`/user/${author?.id === user.id ? "" : author?.id}`}
-			className="text-sm text-teal-500 font-bold hover:underline"
-		>
-			{author?.name}
-		</Link>
+		<div className="flex justify-between items-center text-sm">
+			<Link
+				href={`/user/${author?.id === user.id ? "" : author?.id}`}
+				className="text-teal-500 font-bold hover:underline"
+			>
+				{author?.name}
+			</Link>
+			{author?.id === user.id && (
+				<Link
+					className="flex items-center gap-1 hover:text-teal-500 hover:underline transition-colors"
+					href={`/edit/${postId}`}
+				>
+					<Edit size={14} /> Edit
+				</Link>
+			)}
+		</div>
 	);
 }
