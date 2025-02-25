717812963a908e495457fc6605bcdce410ade655
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-02-25 14:18:19 +0600

    add latest posts card to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
+"use client";
+
+import PostCard from "@/components/postcard/PostCard";
+import PostContext from "@/contexts/PostContext";
+import { useContext } from "react";
+
 export default function Home() {
-	return <div>Home Page</div>;
+	const {getPosts} = useContext(PostContext);
+	const posts = getPosts();
+	return <div className="container min-h-[calc(100vh-4rem)] mx-auto p-5">
+		<h1 className="text-2xl font-bold mb-5 text-center">Latest Posts</h1>
+		<div>
+			{
+				posts.map((post) => (
+					<PostCard key={post.id} {...post} />
+				))
+			}
+		</div>
+	</div>;
 }
