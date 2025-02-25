fe3a860bd71cb97c945320336679a0d7c20162ac
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-02-25 20:41:30 +0600

    if there is a user then show the like and comment btn and also comment form

--- src/components/postcard/PostCard.tsx
+++ src/components/postcard/PostCard.tsx
@@ -1,8 +1,7 @@
 import AuthContext from "@/contexts/AuthContext";
 import PostContext from "@/contexts/PostContext";
 import { Post } from "@/types";
-import { Link } from "lucide-react";
-import { useContext, useState } from "react";
+import { FormEvent, useContext, useState } from "react";
 import PostLikeBtn from "./PostLikeBtn";
 import PostAuthor from "./PostAuthor";
 import PostComments from "./PostComments";
@@ -13,36 +12,59 @@ interface PostCardProps extends Post {
 	isSingleComment?: boolean;
 }
 
-export default function PostCard({ id, title, body, userId, updatedAt, isSingleComment = true }: PostCardProps) {
+export default function PostCard({
+	id,
+	title,
+	body,
+	userId,
+	updatedAt,
+	isSingleComment = true,
+}: PostCardProps) {
 	const [focusComment, setFocusComment] = useState(false);
 	const { getLikes, getComments } = useContext(PostContext);
-	const { getUserById } = useContext(AuthContext);
+	const { getUserById, user } = useContext(AuthContext);
 
 	const author = getUserById(userId);
 	const likes = getLikes(id);
-    const comments = getComments(id);
+	const comments = getComments(id);
+
+	function handleClick(event: FormEvent) {
+		setFocusComment(false);
+	}
 
 	return (
-		<div onClick={() => setFocusComment(false)} className="bg-white rounded-xl shadow p-5 mb-5">
+		<div
+			onClick={handleClick}
+			className="bg-white rounded-xl shadow p-5 mb-5"
+		>
 			<div>
-				<PostAuthor author={author!} />
+				<PostAuthor author={author!} postId={id} />
 			</div>
 			<h1 className="text-xl font-bold">{title}</h1>
 			<p className="mt-2">{body}</p>
-			<p className="text-[0.75rem] text-gray-500 mt-1 text-right">{updatedAt}</p>
+			<p className="text-[0.75rem] text-gray-500 mt-1 text-right">
+				{updatedAt}
+			</p>
 			<div className="flex items-center gap-5 mt-5">
 				<p className="text-sm text-gray-500">{likes.length} likes</p>
-				<p className="text-sm text-gray-500">{comments.length} comments</p>
+				<p className="text-sm text-gray-500">
+					{comments.length} comments
+				</p>
 			</div>
-			<div className="flex items-center gap-5 mt-2">
-				<PostLikeBtn postId={id} />
-				<PostCommentBtn focusComment={focusComment} setFocusComment={setFocusComment} />
-			</div>
+			{user.id && (
+				<div className="flex items-center gap-5 mt-2">
+					<PostLikeBtn postId={id} />
+					<PostCommentBtn
+						focusComment={focusComment}
+						setFocusComment={setFocusComment}
+					/>
+				</div>
+			)}
 			<div className="mt-2">
 				<PostComments isSingle={isSingleComment} comments={comments} />
-				<div className="">
+				{user.id && (
 					<AddComment focusComment={focusComment} postId={id} />
-				</div>
+				)}
 			</div>
 		</div>
 	);
